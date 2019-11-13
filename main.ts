4b26e28f5d53c0d638a1007325016d4fd255f2ab
5rrt <1398535812@qq.com>
2019-11-13 16:47:26 +0800

    改了灯1才亮11

--- main.ts
+++ main.ts
@@ -334,7 +334,7 @@ namespace LANDZO_TS {
     //% blockId="GPIO_Write_Digital" block="|%io|端口数字值写入|%d|"
     //% weight=50
     export function GPIO_Write_Digital(io: IO_DIGITAL_W, d: number) :void {
-        write_byte2(0x03, io, ~d);
+        write_byte2(0x03, io, !d);
     }
     
     /*
